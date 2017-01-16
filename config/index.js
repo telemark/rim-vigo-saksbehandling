c361e7e21600737aa148e38eac968e93f68d9c06
Geir Gåsodden <geir.gasodden@pythonia.no>
2017-01-16 15:27:33 +0100

    Adds archive

--- config/index.js
+++ config/index.js
@@ -4,10 +4,7 @@ const envs = process.env
 
 module.exports = {
   QUEUE_DIRECTORY_PATH: envs.QUEUE_DIRECTORY_PATH || 'test/data/queue',
-  JOBS_DIRECTORY_PATH: envs.JOBS_DIRECTORY_PATH || 'test/data/jobs',
+  ARCHIVE_DIRECTORY_PATH: envs.ARCHIVE_DIRECTORY_PATH || 'test/data/archive',
   ERRORS_DIRECTORY_PATH: envs.ERRORS_DIRECTORY_PATH || 'test/data/errors',
-  DONE_DIRECTORY_PATH: envs.DONE_DIRECTORY_PATH || 'test/data/done',
-  URL: envs.URL || 'https://vigo.dummy.allthethings.win',
-  USERNAME: envs.USERNAME || 'my-username',
-  PASSWORD: envs.PASSWORD || 'my-password'
+  DONE_DIRECTORY_PATH: envs.DONE_DIRECTORY_PATH || 'test/data/done'
 }
