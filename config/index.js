3cdc573f36cf9414328e41e6c36dfabe89eccbe6
Geir Gåsodden <geir.gasodden@pythonia.no>
2017-01-24 14:27:07 +0100

    Adds sending status to firebase

--- config/index.js
+++ config/index.js
@@ -6,5 +6,11 @@ module.exports = {
   ARCHIVE_DIRECTORY_PATH: envs.ARCHIVE_DIRECTORY_PATH || 'test/data/archive',
   DONE_DIRECTORY_PATH: envs.DONE_DIRECTORY_PATH || 'test/data/done',
   ERRORS_DIRECTORY_PATH: envs.ERRORS_DIRECTORY_PATH || 'test/data/errors',
-  QUEUE_DIRECTORY_PATH: envs.QUEUE_DIRECTORY_PATH || 'test/data/queue'
+  QUEUE_DIRECTORY_PATH: envs.QUEUE_DIRECTORY_PATH || 'test/data/queue',
+  fireBase: {
+    apiKey: envs.FIREBASE_API_KEY || 'AIzaSyCLOgh_XMHbOymgY4qBu4Rv9-tZP6xvNAc',
+    databaseURL: envs.FIREBASE_URL || 'https://seneca-firebase-test.firebaseio.com',
+    authEmail: envs.FIREBASE_EMAIL || 'seneca-test@gasodden.net',
+    authPassword: envs.FIREBASE_PASSWORD || 'seneca-test-password'
+  }
 }
